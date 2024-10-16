f5412a03a73f039276cffb62d570c634e4579a09
Chloe Rudge <chloe.rudge@yahoo.co.uk>
2024-10-16 16:45:55 +0100

    introduce new url names as per central auth changes

--- ra-smoke-test-local.sh
+++ ra-smoke-test-local.sh
@@ -1,8 +1,9 @@
 #!/bin/sh -xe
 
 sm2 --stop CENTRALISED_AUTHORISATION_ALL
-#sleep 5
-sm2 --start CENTRALISED_AUTHORISATION_ALL --wait 120 --noprogress \
+sleep 5
+sm2 --start CENTRALISED_AUTHORISATION_ALL --wait 60 --noprogress --appendArgs '{"IDENTITY_PROVIDER_GATEWAY_FRONTEND":["-Didentity-provider-gateway-frontend.features.newRoutesEnabled=true"],
+                                                                                "IDENTITY_PROVIDER_GATEWAY":["-Didentity-provider-gateway.features.newRoutesEnabled=true"]}'
 
 if [ $? != 0 ]
 then
