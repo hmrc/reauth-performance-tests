c8fd529d152eba4e1de3130b18eb621874180ecc
Chloe Rudge <chloe.rudge@yahoo.co.uk>
2024-11-27 10:23:57 +0000

    add new IDP-GW authorise url

--- ra-smoke-test-local.sh
+++ ra-smoke-test-local.sh
@@ -2,9 +2,7 @@
 
 sm2 --stop CENTRALISED_AUTHORISATION_ALL
 sleep 5
-sm2 --start CENTRALISED_AUTHORISATION_ALL --wait 60 --noprogress --appendArgs '{"IDENTITY_PROVIDER_GATEWAY_FRONTEND":["-Didentity-provider-gateway-frontend.features.newRoutesEnabled=true"],
-                                                                                "IDENTITY_PROVIDER_GATEWAY":["-Didentity-provider-gateway.features.newRoutesEnabled=true"]}'
-
+sm2 --start CENTRALISED_AUTHORISATION_ALL --wait 60 --noprogress
 if [ $? != 0 ]
 then
     echo "Failed to start all services"
