bb5ad5053b52c3454008e0e8e80f44d58ec5fae0
James Williams <james.williams1@digital.hmrc.gov.uk>
2025-02-19 13:18:30 +0000

    Update ra-smoke-test-local.sh

--- ra-smoke-test-local.sh
+++ ra-smoke-test-local.sh
@@ -1,8 +1,8 @@
 #!/bin/sh -xe
 
-sm2 --stop CENTRALISED_AUTHORISATION_WITH_CANARY
+sm2 --stop CENTRALISED_AUTHORISATION_ALL
 sleep 5
-sm2 --start CENTRALISED_AUTHORISATION_WITH_CANARY --wait 60 --noprogress
+sm2 --start CENTRALISED_AUTHORISATION_ALL --wait 60 --noprogress
 if [ $? != 0 ]
 then
     echo "Failed to start all services"
